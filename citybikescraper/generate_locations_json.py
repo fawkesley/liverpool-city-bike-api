9ab3dc6db0f64804a1fd80f9ea7f3e76883143ee
Paul M Furley <paul@paulfurley.com>
2014-09-15 10:53:18 +0100

    Generate real lastRefreshed time
    Note that we're still not actually downloading the data yet...

--- citybikescraper/generate_locations_json.py
+++ citybikescraper/generate_locations_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import datetime
 import json
+import pytz
 
 from .get_latest_locations import get_latest_locations
 
@@ -8,9 +10,18 @@ from .get_latest_locations import get_latest_locations
 def generate_locations_json():
     locations = get_latest_locations()
     output_json = json.dumps({
-        'lastRefreshed': '2014-09-11T17:38:00Z',
         'lastChanged': '2014-09-11T17:38:00Z',
+        'lastRefreshed': format_datetime(utc_now()),
         'locations': locations
     }, indent=4)
 
     return output_json
+
+
+def format_datetime(dt):
+    assert dt.tzinfo == pytz.UTC
+    return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+def utc_now():
+    return datetime.datetime.now(pytz.UTC)

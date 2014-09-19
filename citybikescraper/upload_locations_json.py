445760401a65acfed2c34ca3285da440392e6732
Paul M Furley <paul@paulfurley.com>
2014-09-19 16:04:34 +0100

    GZip compress JSON before uploading
    Resulting in 87% reduction in size.

--- citybikescraper/upload_locations_json.py
+++ citybikescraper/upload_locations_json.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import gzip
 import logging
 import os
 import sys
+
+from cStringIO import StringIO
 
 import boto
 
@@ -19,20 +22,29 @@ def main():
 
 
 def upload_locations_json(json_string):
+    gzipped_json = gzip_compress(json_string)
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
 
     conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
     bucket = conn.get_bucket(AWS_BUCKET_NAME)
-
     k = bucket.new_key(AWS_KEY_NAME)
+
     k.content_type = 'application/json'
 
     k.set_contents_from_string(
-        json_string,
+        gzipped_json,
+        headers={'Content-Encoding': 'gzip'},
         replace=True,
         policy='public-read')
+
+
+def gzip_compress(string):
+    out = StringIO()
+    with gzip.GzipFile(fileobj=out, mode='w') as f:
+        f.write(string)
+    return out.getvalue()
 
 
 if __name__ == '__main__':
